2c71e0a5e42cbb0a35f0627a4b12b85bd962d9aa
MuhammadHassanVahand <hassan12rehan@gmail.com>
2023-06-22 18:48:54 +0500

    Update

--- smallest&greatest no.dart
+++ smallest&greatest no.dart
@@ -1,5 +1,5 @@
 void main() {
-  List<int> numbers = [2, 1, 5, 3, 9, 6, 10, 0, 23, 7];
+  List<int> numbers = [2, 1, 5, 3, 9, 6, 10, 0, 100, 23, 7];
   numbers.sort();
   print(numbers.first);
   print(numbers.last);
